8df491995ec7f480603f87bf9f852f6126c48638
ChanwitSS <chanwit.sa@ku.th>
2021-07-30 20:31:46 +0700

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '24303691223c53a1691076fc1ff53151'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8b876d60-e9d5-4b75-920a-4e0f5ba084a3'
+};
